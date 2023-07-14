8ef3884e3f689d63e606eb9387580d671dfd4ab9
Niimphu <nimphu@outlook.com>
2023-07-14 16:42:07 +0200

    moved parser to init.c

--- philo/src/init.c
+++ philo/src/init.c
@@ -12,6 +12,20 @@
 
 #include "../dep/philo.h"
 
+int	parse(int argn, char *arguments[], t_main *data)
+{
+	if (argn < 5 || argn > 6)
+		return (1);
+	data->philo_count = ft_atoi(arguments[1]);
+	data->die_time = ft_atoi(arguments[2]);
+	data->eat_time = ft_atoi(arguments[3]);
+	data->sleep_time = ft_atoi(arguments[4]);
+	data->max_meals = -1;
+	if (arguments[5])
+		data->max_meals = ft_atoi(arguments[5]);
+	return (0);
+}
+
 int	initialise_data(t_main *data)
 {
 	int	i;
