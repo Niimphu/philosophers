57147f2c35eb428a4715b2aeaadfde169dcdb043
Niimphu <nimphu@outlook.com>
2023-07-29 15:55:01 +0200

    fun functions init

---
+++ philo/src/spaghetti.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   spaghetti.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/29 15:01:39 by yiwong            #+#    #+#             */
+/*   Updated: 2023/07/29 15:01:39 by yiwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../dep/philo.h"
+#include "../dep/philo_fun.h"
+
+void	print_food(int time)
+{
+	time = time % 10;
+	if (time == 0)
+		printf("spaghetti\n");
+	else if (time == 1)
+		printf("lasagne\n");
+	else if (time == 2)
+		printf("soup\n");
+	else if (time == 3)
+		printf("sushi\n");
+	else if (time == 4)
+		printf("kebab\n");
+	else if (time == 5)
+		printf("ramen\n");
+	else if (time == 6)
+		printf("borgar\n");
+	else if (time == 7)
+		printf("some cake\n");
+	else if (time == 8)
+		printf("pudding\n");
+	else if (time == 9)
+		printf("a donut\n");
+}
---
+++ philo/src/utils_fun.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yiwong <yiwong@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 21:19:00 by yiwong            #+#    #+#             */
+/*   Updated: 2023/07/28 22:19:37 by yiwong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../dep/philo.h"
+#include "../dep/philo_fun.h"
+
+void	print_time_and_id(t_philo *philo);
+char	*get_action_string(int action);
+void	colour(int action);
+
+int	ft_atoi(const char *string)
+{
+	int	i;
+	int	string_as_int;
+
+	i = 0;
+	string_as_int = 0;
+	while (string[i])
+	{
+		string_as_int = string_as_int * 10 + (string[i] - '0');
+		i++;
+	}
+	return (string_as_int);
+}
+
+int	print_action(t_philo *philo, int action)
+{
+	pthread_mutex_lock(philo->data->print_lock);
+	if (!continue_program(philo->data))
+	{
+		pthread_mutex_unlock(philo->data->print_lock);
+		return (STOP);
+	}
+	if (action == DIE)
+	{
+		pthread_mutex_lock(philo->data->philos_alive_lock);
+		philo->data->all_philos_alive = false;
+		pthread_mutex_unlock(philo->data->philos_alive_lock);
+	}
+	colour(action);
+	print_time_and_id(philo);
+	printf("%s", get_action_string(action));
+	if (action == EAT)
+//		print_food(get_elapsed_time(philo->start_time));
+		printf(" food\n");
+	printf(END);
+	pthread_mutex_unlock(philo->data->print_lock);
+	return (OK);
+}
+
+void	colour(int action)
+{
+	if (action == FORK)
+		printf(YELLOW);
+	else if (action == EAT)
+		printf(BLUE);
+	else if (action == SLEEP)
+		printf(GREEN);
+	else if (action == DIE)
+		printf(RED);
+}
+
+void	print_time_and_id(t_philo *philo)
+{
+	printf("%i %i ", get_elapsed_time(philo->start_time), philo->id);
+}
+
+char	*get_action_string(int action)
+{
+	if (action == FORK)
+		return ("has taken a fork\n");
+	if (action == EAT)
+		return ("is eating ");
+	if (action == SLEEP)
+		return ("is sleeping\n");
+	if (action == THINK)
+		return ("is thinking\n");
+	if (action == DIE)
+		return ("died\n");
+	return (NULL);
+}
